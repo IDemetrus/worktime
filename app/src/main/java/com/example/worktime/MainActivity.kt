47f9b09d5407c7372c26c266ed549e0dc367a3b1
IDemertus <workhome.pro@gmail.com>
2020-11-14 22:09:27 +0300

    add time picker xml

--- app/src/main/java/com/example/worktime/MainActivity.kt
+++ app/src/main/java/com/example/worktime/MainActivity.kt
@@ -1,11 +1,114 @@
 package com.example.worktime
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.Dialog
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.TimePicker
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.example.worktime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar: Toolbar
+    private lateinit var timePicker: TimePicker
+    private lateinit var timer: CountDownTimer
+    private lateinit var timerTv: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.i("[${this.javaClass}]", "-> [onCreate]")
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Init toolbar
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.title = getString(R.string.app_name)
+        setSupportActionBar(toolbar)
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onStart() {
+        super.onStart()
+
+        Log.i("[${this.javaClass}]", "-> [onStart]")
+
+        //Init start/stop buttons
+        val startBtn = binding.timerBtnStart
+        val stopBtn = binding.timerBtnStop
+
+        //Init timer text
+        timerTv = binding.timerTv
+        var timerHour = 0
+        var timerMinute = 45
+
+        timePicker = findViewById(R.id.timerTp)
+        timePicker.setIs24HourView(true)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            timePicker.hour = timerHour
+            timePicker.minute = timerMinute
+        }
+        timePicker.setOnTimeChangedListener { view, hourOfDay, minute ->
+            timerHour = hourOfDay
+            timerMinute = minute
+            Log.i("MainActivity", "$hourOfDay:$minute")
+        }
+
+        startBtn.setOnClickListener {
+            Toast.makeText(this, "${startBtn.text}", Toast.LENGTH_SHORT).show()
+
+            timePicker.visibility = View.INVISIBLE
+            timerTv.visibility = View.VISIBLE
+            timerTv.text = "$timerHour : $timerMinute"
+
+            when (startBtn.text) {
+                "Start" -> startBtn.text = getString(R.string.pause_btn)
+                "Pause" -> startBtn.text = getString(R.string.start_btn)
+            }
+        }
+        stopBtn.setOnClickListener {
+            Toast.makeText(this, "${stopBtn.text}", Toast.LENGTH_SHORT).show()
+            timerTv.visibility = View.INVISIBLE
+            timePicker.visibility = View.VISIBLE
+            startBtn.text = getString(R.string.start_btn)
+        }
+    }
+
+    override fun onCreateDialog(id: Int, args: Bundle?): Dialog? {
+        val builder = androidx.appcompat.app.AlertDialog.Builder(applicationContext)
+        builder.setView(layoutInflater.inflate(R.layout.time_picker, null))
+        return builder.create()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("[${this.javaClass}]", "-> [onResume]")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("[${this.javaClass}]", "-> [onPause]")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("[${this.javaClass}]", "-> [onRestart]")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("[${this.javaClass}]", "-> [onStop]")
+
     }
 }
