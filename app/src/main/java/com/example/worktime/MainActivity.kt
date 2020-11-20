02002a9f3d955fa94b0c4c5900e5acb747888c25
IDemetrus <workhome.pro@gmail.com>
2020-11-20 17:15:01 +0300

    added simple notification

--- app/src/main/java/com/example/worktime/MainActivity.kt
+++ app/src/main/java/com/example/worktime/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.worktime
 
 import android.annotation.SuppressLint
 import android.app.Dialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -13,6 +18,9 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import com.example.worktime.databinding.ActivityMainBinding
@@ -33,5 +41,38 @@ class MainActivity : AppCompatActivity() {
         toolbar = findViewById(R.id.toolbar)
         toolbar.title = getString(R.string.app_name)
         setSupportActionBar(toolbar)
+
+        //TODO set notification from fragment
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        //Notification section
+        var notifBuilder = NotificationCompat.Builder(this, "1")
+            .setSmallIcon(R.drawable.ic_timer_24)
+            .setContentTitle("Timer")
+            .setContentText("Time remain: 30 min")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        with(NotificationManagerCompat.from(this)){
+            notify(1234567890,notifBuilder.build())
+        }
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(){
+        val name = getString(R.string.channel_name)
+        val descriptionText = getString(R.string.channel_desc)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel("1", name, importance).apply {
+            description = descriptionText
+        }
+        //Register channel
+        val notificationManager: NotificationManager = getSystemService(
+            Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
--- app/src/main/java/com/example/worktime/TimerFragment.kt
+++ app/src/main/java/com/example/worktime/TimerFragment.kt
@@ -1,23 +1,25 @@
 package com.example.worktime
 
-import android.content.res.Resources
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
 import android.graphics.Color
-import android.media.AudioManager
 import android.media.MediaPlayer
-import android.media.SoundPool
 import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TimePicker
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
 import com.example.worktime.databinding.FragmentTimerBinding
-import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -27,28 +29,35 @@ class TimerFragment : Fragment() {
     private val binding get() = _binding!!
     private var timer: CountDownTimer? = null
 
+    private var lastTime: Long = 0L
 
     @RequiresApi(Build.VERSION_CODES.M)
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentTimerBinding.inflate(inflater,container,false)
+        _binding = FragmentTimerBinding.inflate(inflater, container, false)
         val rootView = binding.root
 
         //Add media player
-        var mediaPlayer : MediaPlayer? = null
+        var mediaPlayer: MediaPlayer? = null
+
 
         //Init timePicker
         val timePicker = binding.timerTp
         timePicker.setIs24HourView(true)
         timePicker.hour = 0
         timePicker.minute = 45
+        timePicker.setOnTimeChangedListener { view, hourOfDay, minute ->
+            lastTime = getLong(hourOfDay.toString(), minute.toString())
+        }
 
         //Set sound for media player
         Log.i("Time", "Sound button is: ${binding.soundIb.isActivated}")
         binding.soundIb.isActivated = true
         binding.soundIb.setOnClickListener {
-            if (it.isActivated){
+            if (it.isActivated) {
                 Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
                 Log.i("Time", "Sound button is: ${it.isActivated}")
                 it.isActivated = false
@@ -61,7 +70,7 @@ class TimerFragment : Fragment() {
 
         }
         mediaPlayer = if (binding.soundIb.isActivated) {
-            MediaPlayer.create(context, R.raw.beep)
+            MediaPlayer.create(context, R.raw.ship_bell)
         } else {
             mediaPlayer?.release()
             null
@@ -72,56 +81,115 @@ class TimerFragment : Fragment() {
         val simpleDateFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
         simpleDateFormat.timeZone = TimeZone.getTimeZone("UTC")
 
-//        val dateFormat = DateFormat.getDateInstance()
-//        dateFormat.timeZone = TimeZone.getDefault()
         binding.timerBtnStart.setOnClickListener {
-            binding.timerHourTv.visibility = View.GONE
-            binding.timerMinuteTv.visibility = View.GONE
+            binding.timerBtnStop.isEnabled = true
 
             //Changes button text
-            if (binding.timerBtnStart.text == getString(R.string.start_btn)){
+            if (binding.timerBtnStart.text == getString(R.string.start_btn) &&
+                getLong(timePicker.hour.toString(), timePicker.minute.toString()) > 0
+            ) {
                 binding.timerBtnStart.text = getString(R.string.pause_btn)
 
+                binding.timerHourTv.visibility = View.GONE
+                binding.timerMinuteTv.visibility = View.GONE
+
                 mediaPlayer = if (binding.soundIb.isActivated) {
-                    MediaPlayer.create(context, R.raw.beep)
+                    MediaPlayer.create(context, R.raw.ship_bell)
                 } else {
                     mediaPlayer?.release()
                     null
                 }
 
-                timer = object : CountDownTimer(getLong(timePicker.hour.toString(),
-                    timePicker.minute.toString()),1000){
-                    override fun onTick(millisUntilFinished: Long) {
-                        //Color changes before 1 min and 10 seconds timer off
-                        if (millisUntilFinished < 60000){
-                            binding.timerTv.setTextColor(Color.BLACK)
-                            if(millisUntilFinished < 10000){
-                                binding.timerTv.setTextColor(Color.RED)
+                if (lastTime != getLong(
+                        timePicker.hour.toString(),
+                        timePicker.minute.toString()
+                    ) && lastTime > 0
+                ) {
+                    timer = object : CountDownTimer(
+                        lastTime, 1000
+                    ) {
+                        override fun onTick(millisUntilFinished: Long) {
+                            //Color changes before 1 min and 10 seconds timer off
+                            if (millisUntilFinished < 60000) {
+                                binding.timerTv.setTextColor(Color.BLACK)
+                                if (millisUntilFinished < 10000) {
+                                    binding.timerTv.setTextColor(Color.RED)
+                                }
                             }
+                            Log.i("TimerFragment", simpleDateFormat.format((millisUntilFinished)))
+                            binding.timerTv.visibility = View.VISIBLE
+                            timePicker.visibility = View.INVISIBLE
+                            binding.timerTv.text = simpleDateFormat.format(millisUntilFinished)
+
+                            //Set temporary var long time, fot last timer value
+                            lastTime = millisUntilFinished
+
                         }
-                        Log.i("TimerFragment", simpleDateFormat.format((millisUntilFinished)))
-                        binding.timerTv.visibility = View.VISIBLE
-                        timePicker.visibility = View.INVISIBLE
-                        binding.timerTv.text = simpleDateFormat.format(millisUntilFinished)
-                    }
 
-                    override fun onFinish() {
-                        Log.i("TimerFragment", "Timer: Finished")
-                        binding.timerTv.text = getString(R.string.timer_off)
-                        //TODO add play sound
-//                        mediaPlayer?.isLooping = true
-                        mediaPlayer?.start()
+                        override fun onFinish() {
+                            lastTime = 0
+                            Log.i("TimerFragment", "Timer: Finished")
+                            binding.timerTv.text = getString(R.string.timer_off)
+                            //TODO add play sound
+                            mediaPlayer?.isLooping = true
+                            mediaPlayer?.start()
 
-                    }
-                }.start()
+                        }
+                    }.start()
+                } else {
+
+
+                    timer = object : CountDownTimer(
+                        getLong(timePicker.hour.toString(), timePicker.minute.toString()), 1000
+                    ) {
+                        override fun onTick(millisUntilFinished: Long) {
+                            //Color changes before 1 min and 10 seconds timer off
+                            if (millisUntilFinished < 60000) {
+                                binding.timerTv.setTextColor(Color.BLACK)
+                                if (millisUntilFinished < 10000) {
+                                    binding.timerTv.setTextColor(Color.RED)
+                                }
+                            }
+                            Log.i("TimerFragment", simpleDateFormat.format((millisUntilFinished)))
+                            binding.timerTv.visibility = View.VISIBLE
+                            timePicker.visibility = View.INVISIBLE
+                            binding.timerTv.text = simpleDateFormat.format(millisUntilFinished)
+
+                            //Set temporary var long time, fot last timer value
+                            lastTime = millisUntilFinished
+
+                        }
+
+                        override fun onFinish() {
+                            lastTime = 0
+                            Log.i("TimerFragment", "Timer: Finished")
+                            binding.timerTv.text = getString(R.string.timer_off)
+                            mediaPlayer?.isLooping = true
+                            mediaPlayer?.start()
+
+                        }
+                    }.start()
+
+                }
             } else {
+                if (lastTime > 0){
+                    binding.timerBtnStart.text = getString(R.string.start_btn)
+                } else {
+                    Toast.makeText(context, "Set time!",Toast.LENGTH_SHORT).show()
+                }
                 timer?.cancel()
-                //TODO add last timer time to start new* after pause
             }
 
         }
+        //Set stop button disabled
+        //TODO add enable/disable the button
+
         binding.timerBtnStop.setOnClickListener {
             Log.i("TimerFragment", "Timer: Stopped")
+
+            timer?.cancel()
+
+            lastTime = getLong(timePicker.hour.toString(), timePicker.minute.toString())
 
             binding.timerHourTv.visibility = View.VISIBLE
             binding.timerMinuteTv.visibility = View.VISIBLE
@@ -129,11 +197,9 @@ class TimerFragment : Fragment() {
             binding.timerTv.visibility = View.GONE
             timePicker.visibility = View.VISIBLE
             binding.timerBtnStart.text = getString(R.string.start_btn)
-            timer?.cancel()
             mediaPlayer?.stop()
             mediaPlayer = null
         }
-
 
 
 
@@ -151,7 +217,8 @@ class TimerFragment : Fragment() {
     }
 
     private fun getLong(hour: String, minute: String): Long {
-        return ((hour.toInt()*3600*1000)+(minute.toInt()*60*1000)).toLong()
+        return ((hour.toInt() * 3600 * 1000) + (minute.toInt() * 60 * 1000)).toLong()
     }
+
 
 }
